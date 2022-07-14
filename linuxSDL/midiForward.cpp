7795b619d209a221c8c5eb1aee74abdd9c628028
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-07-14 21:50:28 +0200

    midi forward

--- linuxSDL/midiForward.cpp
+++ linuxSDL/midiForward.cpp
@@ -1,27 +1,10 @@
 #include "rtmidi/RtMidi.h"
 
 #include <stdio.h>
+#include <unistd.h>
 
 RtMidiIn *m_in;
 RtMidiOut *m_out;
-
-void midiDisconnect()
-{
-    if (m_in)
-    {
-        m_in->closePort();
-        delete m_in;
-        m_in = NULL;
-    }
-
-    if (m_out)
-    {
-        m_out->closePort();
-        delete m_out;
-        m_out = NULL;
-    }
-    printf("Disconnected\n");
-}
 
 void midiProcessMessage(double _deltatime, std::vector<unsigned char> *_message, void *_userData)
 {
@@ -32,74 +15,69 @@ void midiProcessMessage(double _deltatime, std::vector<unsigned char> *_message,
     m_out->sendMessage(_message);
 }
 
-bool midiConnect()
-{
-    bool in_connected = false;
-
-    m_in = new RtMidiIn();
-    unsigned int nPorts = m_in->getPortCount();
-
-    m_out = new RtMidiOut();
-    nPorts = m_out->getPortCount();
-
-    for (unsigned int i = 0; i < nPorts; i++)
-    {
-        std::string name = m_out->getPortName(i);
-        if (name.rfind("Pico", 0) == 0)
-        {
-            printf("Found Pico MIDI output port: %s\n", name.c_str());
-            try
-            {
-                m_out = new RtMidiOut(RtMidi::Api(0), "opz_dump");
-                m_out->openPort(i, name);
-                // m_out->sendMessage(opz_init_msg()); // opzInitMsg
-                in_connected = true;
-                break;
-            }
-            catch (RtMidiError &error)
-            {
-                error.printMessage();
-            }
-        }
-    }
-
-    if (in_connected)
-    {
-        for (unsigned int i = 0; i < nPorts; i++)
-        {
-            std::string name = m_in->getPortName(i);
-            if (name.rfind("OP-Z", 0) == 0)
-            {
-                printf("Found OP-Z MIDI input port: %s\n", name.c_str());
-                try
-                {
-                    m_in = new RtMidiIn(RtMidi::Api(0), "opz_dump");
-                    m_in->openPort(i, name);
-                    m_in->ignoreTypes(false, false, true);
-                    m_in->setCallback(midiProcessMessage, NULL);
-                    return true;
-                }
-                catch (RtMidiError &error)
-                {
-                    error.printMessage();
-                }
-            }
-        }
-    }
-
-    midiDisconnect();
-    return false;
-}
-
 int main(int argc, char *args[])
 {
     printf(">>>>>>> Midi forward\n");
 
-    midiConnect();
+    unsigned int nPorts;
 
-    printf("\nReading MIDI input ... press <enter> to quit.\n");
-    char input;
-    std::cin.get(input);
-    midiDisconnect();
+    m_in = new RtMidiIn();
+    m_out = new RtMidiOut();
+
+    while (true)
+    {
+        if (!m_out->isPortOpen())
+        {
+            nPorts = m_in->getPortCount();
+            printf("There are %d input ports available.\n", nPorts);
+            for (unsigned int i = 0; i < nPorts; i++)
+            {
+                std::string name = m_out->getPortName(i);
+                if (name.rfind("Pico", 0) == 0)
+                {
+                    printf("Found Pico MIDI output port: %s\n", name.c_str());
+                    try
+                    {
+                        m_out = new RtMidiOut(RtMidi::Api(0), "opz_dump");
+                        m_out->openPort(i, name);
+                        break;
+                    }
+                    catch (RtMidiError &error)
+                    {
+                        error.printMessage();
+                    }
+                }
+            }
+        }
+
+        if (!m_in->isPortOpen())
+        {
+            nPorts = m_in->getPortCount();
+            printf("There are %d output ports available.\n", nPorts);
+            for (unsigned int i = 0; i < nPorts; i++)
+            {
+                std::string name = m_in->getPortName(i);
+                if (name.rfind("OP-Z", 0) == 0)
+                {
+                    printf("Found OP-Z MIDI input port: %s\n", name.c_str());
+                    try
+                    {
+                        m_in = new RtMidiIn(RtMidi::Api(0), "opz_dump");
+                        m_in->openPort(i, name);
+                        m_in->ignoreTypes(false, false, true);
+                        m_in->setCallback(midiProcessMessage, NULL);
+                        break;
+                    }
+                    catch (RtMidiError &error)
+                    {
+                        error.printMessage();
+                    }
+                }
+            }
+        }
+
+        sleep(1);
+    }
+
     return 0;
 }
