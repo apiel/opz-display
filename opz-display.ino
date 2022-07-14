9ac7b19074a4b7df610fada317f6869b49bbb61e
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-07-14 19:53:01 +0200

    display

--- opz-display.ino
+++ opz-display.ino
@@ -14,13 +14,12 @@ U8G2_SSD1306_72X40_ER_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE); // EastR
 #include <Adafruit_TinyUSB.h>
 #include <MIDI.h>
 
+#include "opzDisplay.h"
+
 // USB MIDI object
 Adafruit_USBD_MIDI usb_midi;
 
 MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, MIDI);
-
-#define RENDER_SIZE 100
-char render[3][RENDER_SIZE] = {"", "", ""};
 
 unsigned long lastDraw = 0;
 void draw()
@@ -34,10 +33,10 @@ void draw()
     u8g2.setFontPosTop();
     u8g2.setFontDirection(0);
     u8g2.setFont(u8g2_font_5x8_tf);
-    u8g2.drawStr(0, 0, render[0]);
+    u8g2.drawStr(0, 0, display.line[0]);
     u8g2.setFont(u8g2_font_7x14_tf);
-    u8g2.drawStr(0, 10, render[1]);
-    u8g2.drawStr(0, 24, render[2]);
+    u8g2.drawStr(0, 10, display.line[1]);
+    u8g2.drawStr(0, 24, display.line[2]);
     u8g2.sendBuffer();
   }
 }
@@ -49,9 +48,7 @@ void setup(void)
   Wire.begin();
   u8g2.begin();
 
-  strcpy(render[0], "Connect to OPZ");
-  strcpy(render[1], "OP-Z");
-  strcpy(render[2], "Display");
+  display.set((char *)"Connect OPZ", (char *)"OP-Z", (char *)"Display");
 
 #if defined(ARDUINO_ARCH_MBED) && defined(ARDUINO_ARCH_RP2040)
   // Manual begin() is required on core without built-in support for TinyUSB such as mbed rp2040
@@ -75,302 +72,4 @@ void loop(void)
 {
   MIDI.read();
   draw();
-}
-
-void handleControlChange(byte channel, byte cc, byte value)
-{
-  switch (channel)
-  {
-  case 1:
-    strcpy(render[0], "Kick");
-    break;
-
-  case 2:
-    strcpy(render[0], "Snares");
-    break;
-
-  case 3:
-    strcpy(render[0], "Perc");
-    break;
-
-  case 4:
-    strcpy(render[0], "Sample");
-    break;
-
-  case 5:
-    strcpy(render[0], "Bass");
-    break;
-
-  case 6:
-    strcpy(render[0], "Lead");
-    break;
-
-  case 7:
-    strcpy(render[0], "ARP");
-    break;
-
-  case 8:
-    strcpy(render[0], "Chord");
-    break;
-
-  default:
-    break;
-  }
-
-  switch (cc)
-  {
-  case 1:
-    strcpy(render[1], "P1");
-    break;
-
-  case 2:
-    strcpy(render[1], "P2");
-    break;
-
-  case 3:
-    strcpy(render[1], "Cutoff");
-    break;
-
-  case 4:
-    strcpy(render[1], "Res");
-    break;
-
-  case 5:
-    strcpy(render[1], "Attack");
-    break;
-
-  case 6:
-    strcpy(render[1], "Decay");
-    break;
-
-  case 7:
-    strcpy(render[1], "Substain");
-    break;
-
-  case 8:
-    strcpy(render[1], "Release");
-    break;
-
-  case 9:
-    strcpy(render[1], channel == 7 ? "ARP speed" : "LFO Depth");
-    break;
-
-  case 10:
-    strcpy(render[1], channel == 7 ? "ARP Pattern" : "LFO Rate");
-    break;
-
-  case 11:
-    strcpy(render[1], channel == 7 ? "ARP Style" : "LFO Dest");
-    break;
-
-  case 12:
-    strcpy(render[1], channel == 7 ? "ARP Range" : "LFO Shape");
-    break;
-
-  case 13:
-    strcpy(render[1], "FX Send 1");
-    break;
-
-  case 14:
-    strcpy(render[1], "FX Send 2");
-    break;
-
-  case 15:
-    strcpy(render[1], "Pan");
-    break;
-
-  case 16:
-    strcpy(render[1], "Level");
-    break;
-
-  default:
-    break;
-  }
-
-  if (cc == 12 && channel != 7)
-  {
-    if (value < 10)
-    {
-      strcpy(render[2], "SIN");
-    }
-    else if (value < 21)
-    {
-      strcpy(render[2], "TRI");
-    }
-    else if (value < 32)
-    {
-      strcpy(render[2], "SSQR");
-    }
-    else if (value < 43)
-    {
-      strcpy(render[2], "SAW");
-    }
-    else if (value < 54)
-    {
-      strcpy(render[2], "RND");
-    }
-    else if (value < 64)
-    {
-      strcpy(render[2], "GYRO");
-    }
-    else if (value < 75)
-    {
-      strcpy(render[2], "SIN triggered");
-    }
-    else if (value < 85)
-    {
-      strcpy(render[2], "TRI triggered");
-    }
-    else if (value < 96)
-    {
-      strcpy(render[2], "SSQR triggered");
-    }
-    else if (value < 107)
-    {
-      strcpy(render[2], "SAW triggered");
-    }
-    else if (value < 118)
-    {
-      strcpy(render[2], "RND triggered");
-    }
-    else
-    {
-      strcpy(render[2], "ONCE triggered");
-    }
-  }
-  else if (cc == 11 && channel != 7)
-  {
-    if (value < 16)
-    {
-      strcpy(render[2], "P1");
-    }
-    else if (value < 32)
-    {
-      strcpy(render[2], "P2");
-    }
-    else if (value < 48)
-    {
-      strcpy(render[2], "CUTOFF");
-    }
-    else if (value < 64)
-    {
-      strcpy(render[2], "RES");
-    }
-    else if (value < 80)
-    {
-      strcpy(render[2], "ATTACK");
-    }
-    else if (value < 96)
-    {
-      strcpy(render[2], "PITCH");
-    }
-    else if (value < 112)
-    {
-      strcpy(render[2], "PAN");
-    }
-    else
-    {
-      strcpy(render[2], "VOLUME");
-    }
-  }
-  else if (cc == 10 && channel != 7)
-  {
-    if (value < 8)
-    {
-      strcpy(render[2], "1/64");
-    }
-    else if (value < 16)
-    {
-      strcpy(render[2], "1/32");
-    }
-    else if (value < 24)
-    {
-      strcpy(render[2], "1/16");
-    }
-    else if (value < 32)
-    {
-      strcpy(render[2], "1/8");
-    }
-    else if (value < 40)
-    {
-      strcpy(render[2], "1/4");
-    }
-    else if (value < 48)
-    {
-      strcpy(render[2], "1/2");
-    }
-    else if (value < 56)
-    {
-      strcpy(render[2], "1/1");
-    }
-    else if (value < 64)
-    {
-      strcpy(render[2], "2/1");
-    }
-    else
-    {
-      snprintf(render[2], RENDER_SIZE, "%d Hz", value - 64);
-    }
-  }
-  else if (cc == 9 && channel != 7)
-  {
-    snprintf(render[2], RENDER_SIZE, "%d", value - 64);
-  }
-  else if (cc == 15)
-  {
-    if (value == 64)
-    {
-      strcpy(render[2], "Center");
-    }
-    else
-    {
-      snprintf(render[2], RENDER_SIZE, "%d %s", abs(value - 64), value < 64 ? "Left" : "Right");
-    }
-  }
-  else if (cc == 9 && channel == 7)
-  {
-    if (value == 0)
-    {
-      strcpy(render[2], "Off");
-    }
-    else
-    {
-      snprintf(render[2], RENDER_SIZE, "%d", (int)(value / 16) + 1);
-    }
-  }
-  else if (cc == 10 && channel == 7)
-  {
-    if (value < 21)
-    {
-      strcpy(render[2], "manual");
-    }
-    else if (value < 43)
-    {
-      strcpy(render[2], "up");
-    }
-    else if (value < 64)
-    {
-      strcpy(render[2], "down");
-    }
-    else if (value < 85)
-    {
-      strcpy(render[2], "up/down");
-    }
-    else if (value < 107)
-    {
-      strcpy(render[2], "down/up");
-    }
-    else
-    {
-      strcpy(render[2], "random");
-    }
-  }
-  else if (cc == 11 && channel == 7)
-  {
-    snprintf(render[2], RENDER_SIZE, "%d", (int)(value / 21) + 1);
-  }
-  else
-  {
-    snprintf(render[2], RENDER_SIZE, "%d", value);
-  }
 }
---
+++ opzDisplay.h
@@ -0,0 +1,334 @@
+#ifndef OPZ_DISPLAY_H
+#define OPZ_DISPLAY_H
+
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define RENDER_SIZE 100
+#define LINE_COUNT 3
+
+class OpzDisplay
+{
+public:
+    char line[LINE_COUNT][RENDER_SIZE];
+
+    void set(char *_line1, char *_line2, char *_line3)
+    {
+        strncpy(line[0], _line1, RENDER_SIZE);
+        strncpy(line[1], _line2, RENDER_SIZE);
+        strncpy(line[2], _line3, RENDER_SIZE);
+    }
+
+    void set(uint8_t linePos, char *_line)
+    {
+        strncpy(line[linePos % LINE_COUNT], _line, RENDER_SIZE);
+    }
+
+        void set(uint8_t linePos, const char *_line)
+    {
+        strncpy(line[linePos % LINE_COUNT], _line, RENDER_SIZE);
+    }
+};
+
+OpzDisplay display;
+
+void handleControlChange(uint8_t channel, uint8_t cc, uint8_t value)
+{
+    switch (channel)
+    {
+    case 1:
+        display.set(0, "Kick");
+        break;
+
+    case 2:
+        display.set(0, "Snares");
+        break;
+
+    case 3:
+        display.set(0, "Perc");
+        break;
+
+    case 4:
+        display.set(0, "Sample");
+        break;
+
+    case 5:
+        display.set(0, "Bass");
+        break;
+
+    case 6:
+        display.set(0, "Lead");
+        break;
+
+    case 7:
+        display.set(0, "ARP");
+        break;
+
+    case 8:
+        display.set(0, "Chord");
+        break;
+
+    default:
+        break;
+    }
+
+    switch (cc)
+    {
+    case 1:
+        display.set(1, "P1");
+        break;
+
+    case 2:
+        display.set(1, "P2");
+        break;
+
+    case 3:
+        display.set(1, "Cutoff");
+        break;
+
+    case 4:
+        display.set(1, "Res");
+        break;
+
+    case 5:
+        display.set(1, "Attack");
+        break;
+
+    case 6:
+        display.set(1, "Decay");
+        break;
+
+    case 7:
+        display.set(1, "Substain");
+        break;
+
+    case 8:
+        display.set(1, "Release");
+        break;
+
+    case 9:
+        display.set(1, channel == 7 ? "ARP speed" : "LFO Depth");
+        break;
+
+    case 10:
+        display.set(1, channel == 7 ? "ARP Pattern" : "LFO Rate");
+        break;
+
+    case 11:
+        display.set(1, channel == 7 ? "ARP Style" : "LFO Dest");
+        break;
+
+    case 12:
+        display.set(1, channel == 7 ? "ARP Range" : "LFO Shape");
+        break;
+
+    case 13:
+        display.set(1, "FX Send 1");
+        break;
+
+    case 14:
+        display.set(1, "FX Send 2");
+        break;
+
+    case 15:
+        display.set(1, "Pan");
+        break;
+
+    case 16:
+        display.set(1, "Level");
+        break;
+
+    default:
+        break;
+    }
+
+    if (cc == 12 && channel != 7)
+    {
+        if (value < 10)
+        {
+            display.set(2, "SIN");
+        }
+        else if (value < 21)
+        {
+            display.set(2, "TRI");
+        }
+        else if (value < 32)
+        {
+            display.set(2, "SSQR");
+        }
+        else if (value < 43)
+        {
+            display.set(2, "SAW");
+        }
+        else if (value < 54)
+        {
+            display.set(2, "RND");
+        }
+        else if (value < 64)
+        {
+            display.set(2, "GYRO");
+        }
+        else if (value < 75)
+        {
+            display.set(2, "SIN triggered");
+        }
+        else if (value < 85)
+        {
+            display.set(2, "TRI triggered");
+        }
+        else if (value < 96)
+        {
+            display.set(2, "SSQR triggered");
+        }
+        else if (value < 107)
+        {
+            display.set(2, "SAW triggered");
+        }
+        else if (value < 118)
+        {
+            display.set(2, "RND triggered");
+        }
+        else
+        {
+            display.set(2, "ONCE triggered");
+        }
+    }
+    else if (cc == 11 && channel != 7)
+    {
+        if (value < 16)
+        {
+            display.set(2, "P1");
+        }
+        else if (value < 32)
+        {
+            display.set(2, "P2");
+        }
+        else if (value < 48)
+        {
+            display.set(2, "CUTOFF");
+        }
+        else if (value < 64)
+        {
+            display.set(2, "RES");
+        }
+        else if (value < 80)
+        {
+            display.set(2, "ATTACK");
+        }
+        else if (value < 96)
+        {
+            display.set(2, "PITCH");
+        }
+        else if (value < 112)
+        {
+            display.set(2, "PAN");
+        }
+        else
+        {
+            display.set(2, "VOLUME");
+        }
+    }
+    else if (cc == 10 && channel != 7)
+    {
+        if (value < 8)
+        {
+            display.set(2, "1/64");
+        }
+        else if (value < 16)
+        {
+            display.set(2, "1/32");
+        }
+        else if (value < 24)
+        {
+            display.set(2, "1/16");
+        }
+        else if (value < 32)
+        {
+            display.set(2, "1/8");
+        }
+        else if (value < 40)
+        {
+            display.set(2, "1/4");
+        }
+        else if (value < 48)
+        {
+            display.set(2, "1/2");
+        }
+        else if (value < 56)
+        {
+            display.set(2, "1/1");
+        }
+        else if (value < 64)
+        {
+            display.set(2, "2/1");
+        }
+        else
+        {
+            snprintf(display.line[2], RENDER_SIZE, "%d Hz", value - 64);
+        }
+    }
+    else if (cc == 9 && channel != 7)
+    {
+        snprintf(display.line[2], RENDER_SIZE, "%d", value - 64);
+    }
+    else if (cc == 15)
+    {
+        if (value == 64)
+        {
+            display.set(2, "Center");
+        }
+        else
+        {
+            snprintf(display.line[2], RENDER_SIZE, "%d %s", abs(value - 64), value < 64 ? "Left" : "Right");
+        }
+    }
+    else if (cc == 9 && channel == 7)
+    {
+        if (value == 0)
+        {
+            display.set(2, "Off");
+        }
+        else
+        {
+            snprintf(display.line[2], RENDER_SIZE, "%d", (int)(value / 16) + 1);
+        }
+    }
+    else if (cc == 10 && channel == 7)
+    {
+        if (value < 21)
+        {
+            display.set(2, "manual");
+        }
+        else if (value < 43)
+        {
+            display.set(2, "up");
+        }
+        else if (value < 64)
+        {
+            display.set(2, "down");
+        }
+        else if (value < 85)
+        {
+            display.set(2, "up/down");
+        }
+        else if (value < 107)
+        {
+            display.set(2, "down/up");
+        }
+        else
+        {
+            display.set(2, "random");
+        }
+    }
+    else if (cc == 11 && channel == 7)
+    {
+        snprintf(display.line[2], RENDER_SIZE, "%d", (int)(value / 21) + 1);
+    }
+    else
+    {
+        snprintf(display.line[2], RENDER_SIZE, "%d", value);
+    }
+}
+#endif
